45327154a0184a2febc704ac185a3a61daf0a1fe
mishagin-tim <timerhan999@gmail.com>
2020-07-27 15:40:42 +0300

    Add tests for NPE, empty string

--- CharCounter/src/test/java/com/foxminded/charcounter/CharCounterTest.java
+++ CharCounter/src/test/java/com/foxminded/charcounter/CharCounterTest.java
@@ -1,6 +1,7 @@
 package com.foxminded.charcounter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,28 @@ class CharCounterTest {
 	@BeforeEach
 	void setUp() {
 		charCounter = new CharCounter();
+	}
+
+	@Test
+	void countShouldThrowIllegalArgumentExceptionWhenNullIsPassed() {
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> charCounter.count(null));
+
+		String expected = "Input arguments can't be null";
+
+		String actual = exception.getMessage();
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void countShouldThrowIllegalArgumentExceptionWhenEmptyStringIsPassed() {
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> charCounter.count(""));
+
+		String expected = "Input string can't be empty";
+
+		String actual = exception.getMessage();
+
+		assertEquals(expected, actual);
 	}
 
 	@Test
