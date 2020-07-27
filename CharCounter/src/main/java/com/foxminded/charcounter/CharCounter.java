da5e9227f77400d2a80a484c3ce10bd998ea4e65
mishagin-tim <timerhan999@gmail.com>
2020-07-27 15:41:03 +0300

    Refactor src: add cache

--- CharCounter/src/main/java/com/foxminded/charcounter/CharCounter.java
+++ CharCounter/src/main/java/com/foxminded/charcounter/CharCounter.java
@@ -1,29 +1,56 @@
 package com.foxminded.charcounter;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CharCounter {
+	private Map<String, Map<String, Integer>> listOfAllCharacterInString;
+
+	public CharCounter() {
+		listOfAllCharacterInString = new HashMap<>();
+	}
+
 	public String count(String input) {
+		if (input == null) {
+			throw new IllegalArgumentException("Input arguments can't be null");
+		}
+
+		if (input.isEmpty()) {
+			throw new IllegalArgumentException("Input string can't be empty");
+		}
+
+		Map<String, Integer> numbersOfCharInString = listOfAllCharacterInString.get(input);
+
+		if (numbersOfCharInString != null) {
+			return generateRawRepresentation(numbersOfCharInString);
+		}
+
+		numbersOfCharInString = calculateCharactersOfString(input);
+
+		return generateRawRepresentation(numbersOfCharInString);
+	}
+
+	private Map<String, Integer> calculateCharactersOfString(String str) {
 		Map<String, Integer> numbersOfCharInString = new LinkedHashMap<>();
 
-		String[] chars = input.split("");
+		String[] chars = str.split("");
 
 		for (String it : chars) {
 			numbersOfCharInString.merge(it, 1, (prev, one) -> prev + one);
 		}
 
-		return generateRawRepresentation(numbersOfCharInString);
+		return numbersOfCharInString;
 	}
 
 	private String generateRawRepresentation(Map<String, Integer> numbersOfCharInString) {
-		String result = "";
+		StringBuilder resultString = new StringBuilder("");
 
 		for (Map.Entry<String, Integer> entry : numbersOfCharInString.entrySet()) {
-			result += decorateCharAndNumberForRawRepresentation(entry.getKey(), entry.getValue());
+			resultString.append(decorateCharAndNumberForRawRepresentation(entry.getKey(), entry.getValue()));
 		}
 
-		return result;
+		return resultString.toString();
 	}
 
 	private String decorateCharAndNumberForRawRepresentation(String c, int number) {
